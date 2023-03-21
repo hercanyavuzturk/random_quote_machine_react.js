aaffc461fae21b0b49fa5f0953e81586eebda8b4
Hercan Yavuztürk <hercanyavuzturk@gmail.com>
2023-03-21 18:16:08 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, {useState,useEffect} from 'react';
+import axios from "axios";
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ const [quote,setQuote] = useState("");
+ const [author,setAuthor] = useState("");
+
+ const getQuote = async () => {
+  const response = await axios.get("https://api.quotable.io/random");
+  setQuote(response.data.content);
+  setAuthor(response.data.author);
+
+ };
+
+ useEffect(()=>{
+  getQuote();
+ },[]);
+
+
+
+ return (
+  <div id='quote-box'>
+    <p id='text'>"{quote}"</p>
+    <p id='author'>- {author}</p>
+    <div id='buttons'>
+    <a href="https://twitter.com/intent/tweet" id='tweet-quote' target={'_blank'}>
+  <FontAwesomeIcon icon={faTwitter} />
+     </a>
+    <button id='new-quote' onClick={getQuote}>New Quote</button>
+   
+    
+
+        
     </div>
-  );
-}
+   
+    
+  </div>
+ );
+};
 
 export default App;
